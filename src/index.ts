5af737cb0f6df70369d1d1bb4307f1db41be0b04
Marcelo da Silva <marceloxhenrique@hotmail.com>
2024-02-09 21:52:50 +0100

    feat: add ExpressAdapter to start the server and implements Users, Questions and Answers controllers

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,48 @@
 import LoadEnv from "@infra/helper/LoadEnv";
 import UserEntity from "@domain/entity/UserEntity";
 import UserDAO from "@infra/database/dao/UserDAO";
-import KnexAdapter from "@infra/database/KnexAdapater";
+import KnexAdapter from "@infra/database/KnexAdapter";
 import UUIDGenerator from "@domain/entity/UUIDGenerator";
+import ExpressAdapter from "@infra/http/ExpressAdapter";
+import Registry from "@infra/di/Registry";
+import UserRepositoryImpl from "@infra/database/repository/UserRepositoryImpl";
+import QuestionDAO from "@infra/database/dao/QuestionDAO";
+import QuestionRepositoryImpl from "@infra/database/repository/QuestionRepositoryImpl";
+import AnswerDAO from "@infra/database/dao/AnswerDAO";
+import AnswerRepositoryImpl from "@infra/database/repository/AnswerRepositoryImpl";
 
 LoadEnv.load();
 
-const knexAdapater = new KnexAdapter();
-knexAdapater.connect();
-const userDAO = new UserDAO(knexAdapater.instance);
-userDAO.create({
-  userId: UUIDGenerator.generate(),
-  name: null,
-  email: "test@test.com",
-  password: "123",
-  pictureUrl: null,
-  createdAt: new Date(),
-  updatedAt: null,
-});
+const knexAdapter = new KnexAdapter();
+knexAdapter.connect();
+const userDAO = new UserDAO(knexAdapter.instance);
+const userRepository = new UserRepositoryImpl(userDAO);
+
+const registry = Registry.getInstance();
+registry.register("UserRepository", userRepository);
+
+const questionDAO = new QuestionDAO(knexAdapter.instance);
+const questionRepository = new QuestionRepositoryImpl(questionDAO);
+registry.register("QuestionRepository", questionRepository);
+
+const answerDAO = new AnswerDAO(knexAdapter.instance);
+const answerRepository = new AnswerRepositoryImpl(answerDAO);
+registry.register("AnswerRepository", answerRepository);
+
+const expressAdapter = new ExpressAdapter();
+expressAdapter.listen(3000);
+
+// Route > Controller > UseCase > <Entity>Repository<Model> > DAO
+
+// const knexAdapter = new KnexAdapter();
+// knexAdapter.connect();
+// const userDAO = new UserDAO(knexAdapter.instance);
+// userDAO.create({
+// 	userId: UUIDGenerator.generate(),
+// 	name: null,
+// 	email: 'teste@teste.com',
+// 	password: '123',
+// 	pictureUrl: null,
+// 	createdAt: new Date(),
+// 	updatedAt: null,
+// });
---
+++ src/infra/http/ExpressAdapter.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import "express-async-errors";
+import RouterFactory from "./RouterFactory";
+import HttpServer from "./HttpServer";
+
+export default class ExpressAdapter implements HttpServer {
+  readonly app: express.Application;
+
+  constructor() {
+    this.app = express();
+    this.app.use(cors());
+    this.app.use(bodyParser.json());
+
+    const routerFactory = new RouterFactory();
+
+    this.app.use("/api", routerFactory.register());
+    this.app.use("/api", (req, res) => {
+      res.json({ message: "Hello, World!" });
+    });
+  }
+
+  listen(port: number): void {
+    this.app.listen(port, () => {
+      console.log(`Server running at port ${port}`);
+    });
+  }
+}
